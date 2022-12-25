6f7d0b6c05546e8be7032ecaa4da7d12eec08542
Polina_N <polinanaumez@gmail.com>
2022-12-25 22:45:39 +0200

    changed task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,27 @@
+"use strict";
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
   'Garlic',
-  'Tomatos',
+  'Tomatoes',
   'Herbs',
   'Condiments',
 ];
+
+let ulList = document.getElementById("ingredients");
+ulList.classList.add("item");
+for (let ingredient of ingredients){
+  const itemEl = document.createElement("li");
+  itemEl.textContent= ingredient;
+  ulList.appendChild(itemEl);
+}
+
+console.log(ingredients);
+
+//Напиши скрипт, який для кожного елемента масиву ingredients:
+
+//Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//Додасть назву інгредієнта як його текстовий вміст.
+//Додасть елементу клас item.
+//Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
